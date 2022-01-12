26e0ad33483f7b4df6d77ee011434f60b923ebb2
Sonny Li <sonnynomnom@gmail.com>
2022-01-11 23:49:03 -0800

    draft

---
+++ public/js/queue.js
@@ -0,0 +1,23 @@
+// const queueList = document.getElementsByClassName('queue-list')[0];
+
+// const displayQueue = (songs) => {
+//   const htmlString = songs
+//     .map((song) => {
+//       return `
+//         <div class="queue-title">${song}</div>
+//         <div class="queue-artist">${song.artist}</div>
+//       </button>
+//     `;
+//     })
+//     .join('');
+//   queueList.innerHTML = htmlString;
+// };
+
+// displayQueue(playlist);
+
+    // <img 
+        //   src="${song.album_image}"
+        //   alt="${song.album}"
+        //   width="600"
+        //   height="400">
+        // </img>
