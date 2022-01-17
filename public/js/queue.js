898980ed127ea2fa9e35ad72e78b4a47e2637baa
Sonny Li <sonnynomnom@gmail.com>
2022-01-17 15:11:41 -0800

    Update queue.js according to Aaron's feedback

--- public/js/queue.js
+++ public/js/queue.js
@@ -2,27 +2,25 @@ const queueList = document.getElementsByClassName('queue-list')[0];
 
 const displayQueue = (songs) => {
   const htmlString = songs
-    .map((song) => {
-      return `
-      <div class="queue-song">
-        <div class="queue-album">
-          <img
-            src=${song.album_image}
-            alt=${song.album}
-          />
-          <div class="queue-album-circle"></div>
-        </div>
-        <div class="queue-group">
-          <div>
-            <div class="queue-title">${song.title}</div>
-            <div class="queue-artist">${song.artist}</div>
-          </div>
-          <div class="queue-time">${song.length}</div>
-        </div>
+  .map(({ album, album_image, length, title, artist }) => `
+    <div class="queue-song">
+      <div class="queue-album">
+        <img
+          src=${album_image}
+          alt=${album}
+        />
+        <div class="queue-album-circle"></div>
       </div>
-    `;
-    })
-    .join('');
+      <div class="queue-group">
+        <div>
+          <div class="queue-title">${title}</div>
+          <div class="queue-artist">${artist}</div>
+        </div>
+        <div class="queue-time">${length}</div>
+      </div>
+    </div>
+  `)
+  .join('');
   queueList.innerHTML = htmlString;
 };
 
