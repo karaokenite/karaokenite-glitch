62f1fd24a76da1331f288d97e80f92568f8693d2
Sonny Li <sonnynomnom@gmail.com>
2022-02-11 23:41:08 -0500

    Update sockittome.js

--- sockittome.js
+++ sockittome.js
@@ -1,3 +1,4 @@
+import { lastIndexOf } from 'lodash';
 import { createStore } from 'redux'
 
 module.exports = (io) => {
@@ -20,304 +21,306 @@ module.exports = (io) => {
 
 var rooms = {};
 
-// Socket event constants.
-const SE_INIT = "Init",
-  SE_NEW_USER_ADDED = "NewUserAdded",
-  SE_EXISTING_USER_NOTIFY = "ExistingUserNotify",
-  SE_USER_REMOVED = "UserRemoved",
-  SE_DISCONNECT = "disconnect", // Do not change this name because it is a system event.
-  SE_SIGNAL = "Signal",
-  SE_GET_PLAY_INFO = "GetPlayInfo",
-  SE_PLAY = "Play",
-  SE_PAUSE = "Pause",
-  SE_PREV = "Prev",
-  SE_NEXT = "Next",
-  SE_ADD_SONG_TO_PLAYLIST = "AddSongToPlaylist",
-  SE_ADD_SONG_TO_QUEUE = "AddSongToQueue";
+// // Socket event constants.
+// const SE_INIT = "Init",
+//   SE_NEW_USER_ADDED = "NewUserAdded",
+//   SE_EXISTING_USER_NOTIFY = "ExistingUserNotify",
+//   SE_USER_REMOVED = "UserRemoved",
+//   SE_DISCONNECT = "disconnect", // Do not change this name because it is a system event.
+//   SE_SIGNAL = "Signal",
+//   SE_GET_PLAY_INFO = "GetPlayInfo",
+//   SE_PLAY = "Play",
+//   SE_PAUSE = "Pause",
+//   SE_PREV = "Prev",
+//   SE_NEXT = "Next",
+//   SE_ADD_SONG_TO_PLAYLIST = "AddSongToPlaylist",
+//   SE_ADD_SONG_TO_QUEUE = "AddSongToQueue";
 
-function isEmpty(obj) {
-  for (var prop in obj) {
-    if (obj.hasOwnProperty(prop)) return false;
-  }
+// function isEmpty(obj) {
+//   for (var prop in obj) {
+//     if (obj.hasOwnProperty(prop)) return false;
+//   }
 
-  return true;
-}
+//   return true;
+// }
 
-function getKeyCount(obj) {
-  let count = 0;
+// function getKeyCount(obj) {
+//   let count = 0;
 
-  for (let prop in obj) {
-    if (obj.hasOwnProperty(prop)) 
-      count++;
-  }
+//   for (let prop in obj) {
+//     if (obj.hasOwnProperty(prop)) 
+//       count++;
+//   }
 
-  return count;
-}
+//   return count;
+// }
 
-function getFirstValue(obj) {
-  for (let prop in obj) {
-    if (obj.hasOwnProperty(prop)) 
-      return obj[prop];
-  }
+// function getFirstValue(obj) {
+//   for (let prop in obj) {
+//     if (obj.hasOwnProperty(prop)) 
+//       return obj[prop];
+//   }
 
-  return null;
-}
+//   return null;
+// }
 
-module.exports = (io) => {
-  io.of("/admin").on("connect", (socket) => {
-    console.log(`Client ${socket.id} is connected.`);
-    const roomName = "singsing";
-    const userName = "cooldude";
-    // Join to room.
+// module.exports = (io) => {
+//   io.of("/admin").on("connect", (socket) => {
+//     console.log(`Client ${socket.id} is connected.`);
+//     const roomName = "singsing";
+//     const userName = "cooldude";
+//     // Join to room.
 
-    socket.join(roomName);
+//     socket.join(roomName);
     
-    if (!rooms[roomName]) {
-      rooms[roomName] = {
-        socketUserMap: {},
-        playInfo: {
-          playlist: [],
-          queue: [],
-          currentPlayingIndex: 0,
-          currentPlayingTime: 0,
-          isPlaying: false,
-        },
-      };
-    }
+//     if (!rooms[roomName]) {
+//       rooms[roomName] = {
+//         socketUserMap: {},
+//         playInfo: {
+//           playlist: [],
+//           queue: [],
+//           currentPlayingIndex: 0,
+//           currentPlayingTime: 0,
+//           isPlaying: false,
+//         },
+//       };
+//     }
 
-    console.log(`Client ${socket.id} joined to room ${roomName}`);
+//     console.log(`Client ${socket.id} joined to room ${roomName}`);
 
-    let firstUser = getFirstValue(rooms[roomName].socketUserMap);
+//     let firstUser = getFirstValue(rooms[roomName].socketUserMap);
 
-    // Initiate the connection process as soon as the client connects
-    rooms[roomName].socketUserMap[socket.id] = {
-      socket: socket,
-      userName: userName,
-    };
+//     // Initiate the connection process as soon as the client connects
+//     rooms[roomName].socketUserMap[socket.id] = {
+//       socket: socket,
+//       userName: userName,
+//     };
     
-    // Initialize
-    socket.on(SE_GET_PLAY_INFO, (sender, playInfo) => {
-      rooms[roomName].playInfo.isPlaying = playInfo.isPlaying;
-      rooms[roomName].playInfo.currentPlayingIndex = playInfo.currentPlayingIndex;
-      rooms[roomName].playInfo.currentPlayingTime = playInfo.currentPlayingTime;
+//     // Initialize
+//     socket.on(SE_GET_PLAY_INFO, (sender, playInfo) => {
+//       rooms[roomName].playInfo.isPlaying = playInfo.isPlaying;
+//       rooms[roomName].playInfo.currentPlayingIndex = playInfo.currentPlayingIndex;
+//       rooms[roomName].playInfo.currentPlayingTime = playInfo.currentPlayingTime;
 
-      if (rooms[roomName].socketUserMap[sender])
-        rooms[roomName].socketUserMap[sender].socket.emit(
-          SE_INIT,
-          rooms[roomName].playInfo
-        );
-    });
+//       if (rooms[roomName].socketUserMap[sender])
+//         rooms[roomName].socketUserMap[sender].socket.emit(
+//           SE_INIT,
+//           rooms[roomName].playInfo
+//         );
+//     });
 
-    // Get play info.
-    if (firstUser) {
-      firstUser.socket.emit(SE_GET_PLAY_INFO, socket.id);
-    } else {
-      socket.emit(SE_INIT, rooms[roomName].playInfo);
-    }
+//     // Get play info.
+//     if (firstUser) {
+//       firstUser.socket.emit(SE_GET_PLAY_INFO, socket.id);
+//     } else {
+//       socket.emit(SE_INIT, rooms[roomName].playInfo);
+//     }
 
-    // Asking all other clients to setup the peer connection receiver
-    socket.to(roomName).emit(SE_NEW_USER_ADDED, socket.id, userName);
+//     // Asking all other clients to setup the peer connection receiver
+//     socket.to(roomName).emit(SE_NEW_USER_ADDED, socket.id, userName);
 
-    // Send message to client to initiate a connection
-    // The sender has already setup a peer connection receiver
-    socket.on(SE_EXISTING_USER_NOTIFY, (init_socket_id, existingUserName) => {
-      console.log(
-        "EXISTING USER NOTIFY by " + socket.id + " for " + init_socket_id
-      );
-      rooms[roomName].socketUserMap[init_socket_id].socket.emit(
-        SE_EXISTING_USER_NOTIFY,
-        socket.id,
-        existingUserName
-      );
-    });
+//     // Send message to client to initiate a connection
+//     // The sender has already setup a peer connection receiver
+//     socket.on(SE_EXISTING_USER_NOTIFY, (init_socket_id, existingUserName) => {
+//       console.log(
+//         "EXISTING USER NOTIFY by " + socket.id + " for " + init_socket_id
+//       );
+//       rooms[roomName].socketUserMap[init_socket_id].socket.emit(
+//         SE_EXISTING_USER_NOTIFY,
+//         socket.id,
+//         existingUserName
+//       );
+//     });
 
-    // Relay a peerconnection signal to a specific socket
-    socket.on(SE_SIGNAL, (data) => {
-      // console.log('sending signal from ' + socket.id + ' to ', data);
-      console.log(
-        `Client ${socket.id} in room ${roomName} emitted 'Signal' event.`
-      );
+//     // Relay a peerconnection signal to a specific socket
+//     socket.on(SE_SIGNAL, (data) => {
+//       // console.log('sending signal from ' + socket.id + ' to ', data);
+//       console.log(
+//         `Client ${socket.id} in room ${roomName} emitted 'Signal' event.`
+//       );
 
-      if (!rooms[roomName].socketUserMap[data.socket_id]) {
-        return;
-      }
+//       if (!rooms[roomName].socketUserMap[data.socket_id]) {
+//         return;
+//       }
 
-      rooms[roomName].socketUserMap[data.socket_id].socket.emit(SE_SIGNAL, {
-        socket_id: socket.id,
-        signal: data.signal,
-      });
-    });
+//       rooms[roomName].socketUserMap[data.socket_id].socket.emit(SE_SIGNAL, {
+//         socket_id: socket.id,
+//         signal: data.signal,
+//       });
+//     });
 
-    // Remove the disconnected peer connection from all other connected clients
-    socket.on(SE_DISCONNECT, () => {
-      console.log("socket disconnected " + socket.id);
-      socket.to(roomName).emit(SE_USER_REMOVED, socket.id);
-      delete rooms[roomName].socketUserMap[socket.id];
+//     // Remove the disconnected peer connection from all other connected clients
+//     socket.on(SE_DISCONNECT, () => {
+//       console.log("socket disconnected " + socket.id);
+//       socket.to(roomName).emit(SE_USER_REMOVED, socket.id);
+//       delete rooms[roomName].socketUserMap[socket.id];
 
-      // Delete room if there's no socket.
-      if (isEmpty(rooms[roomName].socketUserMap)) {
-        delete rooms[roomName];
-        console.log(`Room ${roomName} was deleted because no one is in it.`);
-      }
-    });
+//       // Delete room if there's no socket.
+//       if (isEmpty(rooms[roomName].socketUserMap)) {
+//         delete rooms[roomName];
+//         console.log(`Room ${roomName} was deleted because no one is in it.`);
+//       }
+//     });
 
-    // Triggered when a user presses the play button
-    socket.on(SE_PLAY, function () {
-      console.log(
-        `Client ${socket.id} in room ${roomName} emitted 'Play' event.`
-      );
+//     // Triggered when a user presses the play button
+//     socket.on(SE_PLAY, function () {
+//       console.log(
+//         `Client ${socket.id} in room ${roomName} emitted 'Play' event.`
+//       );
 
-      rooms[roomName].playInfo.isPlaying = true;
+//       rooms[roomName].playInfo.isPlaying = true;
 
-      socket.to(roomName).emit(SE_PLAY);
-    });
+//       socket.to(roomName).emit(SE_PLAY);
+//     });
 
-    // Triggered when a user presses the pause button
-    socket.on(SE_PAUSE, function () {
-      console.log(
-        `Client ${socket.id} in room ${roomName} emitted 'Pause' event.`
-      );
+//     // Triggered when a user presses the pause button
+//     socket.on(SE_PAUSE, function () {
+//       console.log(
+//         `Client ${socket.id} in room ${roomName} emitted 'Pause' event.`
+//       );
 
-      rooms[roomName].playInfo.isPlaying = false;
+//       rooms[roomName].playInfo.isPlaying = false;
 
-      socket.to(roomName).emit(SE_PAUSE);
-    });
+//       socket.to(roomName).emit(SE_PAUSE);
+//     });
 
-    // Triggered when a user presses the "next" button
-    socket.on(SE_NEXT, function () {
-      console.log(
-        `Client ${socket.id} in room ${roomName} emitted 'Next' event.`
-      );
+//     // Triggered when a user presses the "next" button
+//     socket.on(SE_NEXT, function () {
+//       console.log(
+//         `Client ${socket.id} in room ${roomName} emitted 'Next' event.`
+//       );
 
-      rooms[roomName].playInfo.currentPlayingIndex++;
-      if (
-        rooms[roomName].playInfo.currentPlayingIndex >
-        rooms[roomName].playInfo.playlist.length - 1
-      )
-        rooms[roomName].playInfo.currentPlayingIndex =
-          rooms[roomName].playInfo.playlist.length - 1;
+//       rooms[roomName].playInfo.currentPlayingIndex++;
+//       if (
+//         rooms[roomName].playInfo.currentPlayingIndex >
+//         rooms[roomName].playInfo.playlist.length - 1
+//       )
+//         rooms[roomName].playInfo.currentPlayingIndex =
+//           rooms[roomName].playInfo.playlist.length - 1;
 
-      socket.to(roomName).emit(SE_NEXT);
-    });
+//       socket.to(roomName).emit(SE_NEXT);
+//     });
 
-    // Triggered when a user presses the "previous" button
-    socket.on(SE_PREV, function () {
-      console.log(
-        `Client ${socket.id} in room ${roomName} emitted 'Prev' event.`
-      );
+//     // Triggered when a user presses the "previous" button
+//     socket.on(SE_PREV, function () {
+//       console.log(
+//         `Client ${socket.id} in room ${roomName} emitted 'Prev' event.`
+//       );
 
-      rooms[roomName].playInfo.currentPlayingIndex--;
-      if (rooms[roomName].playInfo.currentPlayingIndex < 0)
-        rooms[roomName].playInfo.currentPlayingIndex = 0;
+//       rooms[roomName].playInfo.currentPlayingIndex--;
+//       if (rooms[roomName].playInfo.currentPlayingIndex < 0)
+//         rooms[roomName].playInfo.currentPlayingIndex = 0;
 
-      socket.to(roomName).emit(SE_PREV);
-    });
+//       socket.to(roomName).emit(SE_PREV);
+//     });
 
-    // Triggered when a user presses the "Choose Song" button
-    socket.on(SE_ADD_SONG_TO_PLAYLIST, function (songUrl) {
-      console.log(
-        `Client ${socket.id} in room ${roomName} emitted 'addSongToPlaylist' event with ${songUrl}`
-      );
+//     // Triggered when a user presses the "Choose Song" button
+//     socket.on(SE_ADD_SONG_TO_PLAYLIST, function (songUrl) {
+//       console.log(
+//         `Client ${socket.id} in room ${roomName} emitted 'addSongToPlaylist' event with ${songUrl}`
+//       );
 
-      rooms[roomName].playInfo.playlist.push(songUrl);
+//       rooms[roomName].playInfo.playlist.push(songUrl);
 
-      socket.to(roomName).emit(SE_ADD_SONG_TO_PLAYLIST, songUrl);
-    });
+//       socket.to(roomName).emit(SE_ADD_SONG_TO_PLAYLIST, songUrl);
+//     });
 
-    // Triggered when a user presses the "Choose Song" button
-    socket.on(SE_ADD_SONG_TO_QUEUE, function (queueSong) {
-      console.log(
-        `Client ${socket.id} in room ${roomName} emitted 'addSongToQueue' event with ${queueSong["title"]}`
-      );
+//     // Triggered when a user presses the "Choose Song" button
+//     socket.on(SE_ADD_SONG_TO_QUEUE, function (queueSong) {
+//       console.log(
+//         `Client ${socket.id} in room ${roomName} emitted 'addSongToQueue' event with ${queueSong["title"]}`
+//       );
 
-      rooms[roomName].playInfo.queue.push(queueSong);
+//       rooms[roomName].playInfo.queue.push(queueSong);
 
-      socket.to(roomName).emit(SE_ADD_SONG_TO_QUEUE, queueSong);
-    });
-  });
-};
+//       socket.to(roomName).emit(SE_ADD_SONG_TO_QUEUE, queueSong);
+//     });
+//   });
+// };
 
 // Redux Store
 
-const state = {
-  rooms: {
-    1: {
-      socketUserMap: {},
-      playInfo: {
-        playlist: [],
-        queue: [],
-        currentPlayingIndex: 0,
-        currentPlayingTime: 0,
-        isPlaying: false,
-      },
-    },
-    ids: [1],
-  },
-}
+// const state = {
+//   rooms: {
+//     1: {
+//       socketUserMap: {},
+//       playInfo: {
+//         playlist: [],
+//         queue: [],
+//         currentPlayingIndex: 0,
+//         currentPlayingTime: 0,
+//         isPlaying: false,
+//       },
+//     },
+//     ids: [1],
+//   },
+// }
 
-// Redux Actions
-
-const songAddedAction = (roomName, song) => ({
-  type: "SONG_ADDED",
-  payload: {
-    roomnName,
-    song
-  }
-});
-
-const playPressedAction = {
-  type: "PLAY_PRESSED",
-  payload: "Play button pressed",
-};
-
-const pausePressedAction = {
-  type: "PAUSE_PRESSED",
-  payload: "Pause button pressed",
-};
-
-const prevPressedAction = {
-  type: "PREV_PRESSED",
-  payload: "Previous button pressed",
-};
-
-const nextPressedAction = {
-  type: "NEXT_PRESSED",
-  payload: "Next button pressed",
-};
+// const state = [
+//   {
+//     socketUserMap: {},
+//     playlist: [],
+//     queue: [],
+//     currentPlayingIndex: 0,
+//     currentPlayingTime: 0,
+//     isPlaying: false,
+//   },
+//   // ],
+//   // currentUser: {}
+// ]
 
 // Redux Reducers
+let lastId = 0;
 
-const playlistReducer = (state, action) => {
+const SONG_ADDED = "songAdded";
+const PLAY_PRESSED = "playPressed";
+const PAUSE_PRESSED = "pausePressed";
+const PREV_PRESSED = "prevPressed";
+const NEXT_PRESSED = "nextPressed";
+
+export default function playlistReducer(state = [], action) {
   switch (action.type) {
-    // case "SONG_ADDED": {
-    //   return {
-    //     ...rooms,
-    //     [roomName].playInfo.playlist = song;
-    //   };
-
-    // }
+    case "SONG_ADDED":
+      return [
+        ...state,
+        {
+          playlist: action.payload.song,
+        }
+      ];
     case "PLAY_PRESSED": {
-      return {
-        isPlaying: state.rooms[roomName].playInfo.isPlaying = true,
-      };
+      return [
+        ...state,
+        {
+          isPlaying: true,
+        }
+      ];
     }
     case "PAUSE_PRESSED": {
-      return {
-        isPlaying: state.rooms[roomName].playInfo.isPlaying = false,
-      };
+      return [
+        ...state,
+        {
+          isPlaying: false,
+        }
+      ];
     }
     case "PREV_PRESSED": {
-      return {
-        currentPlayingIndex: state.currentPlayingIndex--,
-      };
+      return [
+        ...state,
+        {
+          currentPlayingIndex: state.currentPlayingIndex--,
+        }
+      ];
     }
     case "NEXT_PRESSED": {
-      return {
-        currentPlayingIndex: state.currentPlayingIndex++,
-      };
+      return [
+        ...state,
+        {
+          currentPlayingIndex: state.currentPlayingIndex++,
+        }
+      ];
     }
     default:
-      return state
+      return state;
   }
 };
 
@@ -347,19 +350,54 @@ const playlistReducer = (state, action) => {
 //   }
 // };
 
-let store = createStore(playlistReducer)
-export default store;
+const store = createStore(playlistReducer)
+// export default store;
 
 console.log(store)
 
-store.subscribe(() => {
+
+// Redux Actions
+
+export const songAdded = song => ({
+  type: "SONG_ADDED",
+  payload: {
+    song
+  }
+})
+
+const playPressed = {
+  type: "PLAY_PRESSED",
+  payload: "Play button pressed",
+};
+
+const pausePressed = {
+  type: "PAUSE_PRESSED",
+  payload: "Pause button pressed",
+};
+
+const previousPressed = {
+  type: "PREV_PRESSED",
+  payload: "Previous button pressed",
+};
+
+const nextPressed = {
+  type: "NEXT_PRESSED",
+  payload: "Next button pressed",
+};
+
+
+
+const unsubscribe = store.subscribe(() => {
   console.log("Store changed!", store.getState());
 })
 
 
 store.dispatch({ 
-  type: 'SONG_ADDED' 
-})
+  type: "SONG_ADDED",
+  payload: {
+    song: "https://my-site.example-cdn.com/blah.mp3"
+  }
+});
 // {value: 1}
 
 store.dispatch({ 
@@ -369,10 +407,5 @@ store.dispatch({
 
 store.dispatch({ 
   type: 'PAUSE_PRESSED' 
-})
-// {value: 1}
-
-store.dispatch({ 
-  type: 'SONG_ADDED' 
 })
 // {value: 1}
